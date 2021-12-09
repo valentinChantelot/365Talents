d6b2e5f251c20cd12fa834e15b3d990c8431a713
Valentin <valentin@web-id.fr>
2021-12-09 21:10:36 +0100

    Add about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,6 +1,25 @@
 <template>
-    <div class="about">
-        <h1>This is the about page</h1>
+    <div class="about-page wrapper">
+        <h1>About this app</h1>
+        <article>
+            <h2>Why this app ?</h2>
+            <p>
+                This application was produced by Valentin Chantelot as part of a
+                technical test for 365Talents.
+            </p>
+
+            <h2>Objective</h2>
+            <p>
+                Display dynamically scraped information on a given company on a
+                web page.
+            </p>
+
+            <h2>Thanks</h2>
+            <p>
+                Thanks to <a href="https://undraw.co/">Undraw</a> for the
+                usefull illustration kit.
+            </p>
+        </article>
     </div>
 </template>
 
@@ -9,3 +28,23 @@ export default {
     name: "About",
 }
 </script>
+
+<style lang="scss" scoped>
+.about-page {
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: $dark;
+
+    h2 {
+        font-size: 2.5rem;
+        font-weight: 700;
+    }
+
+    p {
+        margin-bottom: 2rem;
+    }
+}
+</style>
