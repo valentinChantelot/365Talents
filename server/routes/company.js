01077c060994ae278c22c509370be3c204bbd3df
Valentin <valentin@web-id.fr>
2021-12-09 20:26:05 +0100

    Fix url builder

--- server/routes/company.js
+++ server/routes/company.js
@@ -4,14 +4,14 @@ import { Scraper } from "../scraper/scraper.js"
 const router = express.Router()
 
 router.post("/", async (req, res) => {
-    const companyName = req.body.company
+    const companyName = req.body.company.trim()
 
     if (!companyName) {
         res.status(400).json("Company name is required")
         return
     }
 
-    const url = `https://www.linkedin.com/company/${companyName}/`
+    const url = `https://www.linkedin.com/company/${companyName}/about`
     const data = await Scraper(url)
     data.error ? res.status(500).json(data) : res.status(200).json(data)
 })
